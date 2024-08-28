f3036aec2154e44b6756bc86f1e3e29d6cc498a6
egoriottos <titkov1993@bk.ru>
2024-08-28 15:33:22 +0300

    library second com

---
+++ onlineOrders/src/main/java/org/example/onlineorders/OnlineOrdersApplication.java
@@ -0,0 +1,13 @@
+package org.example.onlineorders;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineOrdersApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnlineOrdersApplication.class, args);
+    }
+
+}
---
+++ onlineOrders/src/test/java/org/example/onlineorders/OnlineOrdersApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.onlineorders;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineOrdersApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
